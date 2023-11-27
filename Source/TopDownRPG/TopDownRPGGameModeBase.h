5b6564112f3842fb4ed3a87e20cd418ce371b53e
kpodsiadlo7 <104767107+kpodsiadlo7@users.noreply.github.com>
2023-11-27 17:42:49 +0100

    Initial project files

---
+++ Source/TopDownRPG/TopDownRPGGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TopDownRPGGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOPDOWNRPG_API ATopDownRPGGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
