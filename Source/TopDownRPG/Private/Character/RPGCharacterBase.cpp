2c6994e7665b931a2bcdf28876b4a45b7ae7c60f
kpodsiadlo7 <104767107+kpodsiadlo7@users.noreply.github.com>
2023-11-27 18:40:28 +0100

    create own character and enemy classes

--- Source/TopDownRPG/Private/Character/RPGCharacterBase.cpp
+++ Source/TopDownRPG/Private/Character/RPGCharacterBase.cpp
@@ -1,13 +1,13 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "RPGCharacterBase.h"
+#include "Character/RPGCharacterBase.h"
 
 // Sets default values
 ARPGCharacterBase::ARPGCharacterBase()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
 }
 
@@ -18,17 +18,4 @@ void ARPGCharacterBase::BeginPlay()
 	
 }
 
-// Called every frame
-void ARPGCharacterBase::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
-}
-
-// Called to bind functionality to input
-void ARPGCharacterBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
-{
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
-}
 
---
+++ Source/TopDownRPG/Public/Character/AuraCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/RPGCharacterBase.h"
+#include "AuraCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOPDOWNRPG_API AAuraCharacter : public ARPGCharacterBase
+{
+	GENERATED_BODY()
+	
+};
