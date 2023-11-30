ad3ae68cc3d01bf85f606a8cd4c4827e731e0aae
kpodsiadlo7 <104767107+kpodsiadlo7@users.noreply.github.com>
2023-11-30 23:45:17 +0100

    creating PlayerState - GameplayAbilitySystem and set blueprint version in GameMode - PlayerStateClass

---
+++ Source/TopDownRPG/Private/Player/RPGPlayerState.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/RPGPlayerState.h"
+
+ARPGPlayerState::ARPGPlayerState()
+{
+	// Jak często serwer będzie aktualizował klientów
+	NetUpdateFrequency = 100.f;
+}
