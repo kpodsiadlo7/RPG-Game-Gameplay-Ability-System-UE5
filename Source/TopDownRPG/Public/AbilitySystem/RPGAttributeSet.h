898d937f9be95ec981a62c7cc72de906bf74f17f
kpodsiadlo7 <104767107+kpodsiadlo7@users.noreply.github.com>
2023-12-03 14:57:48 +0100

    creating basic attribute health and mana

--- Source/TopDownRPG/Public/AbilitySystem/RPGAttributeSet.h
+++ Source/TopDownRPG/Public/AbilitySystem/RPGAttributeSet.h
@@ -3,15 +3,48 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "AbilitySystemComponent.h"
 #include "AttributeSet.h"
 #include "RPGAttributeSet.generated.h"
 
-/**
- * 
- */
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
+
 UCLASS()
 class TOPDOWNRPG_API URPGAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
+
+public:
+	URPGAttributeSet();
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Vital Attributes", ReplicatedUsing=OnRep_Health)
+	FGameplayAttributeData Health;
 	
+	UPROPERTY(BlueprintReadOnly, Category = "Vital Attributes", ReplicatedUsing=OnRep_MaxHealth)
+	FGameplayAttributeData MaxHealth;
+	
+	UPROPERTY(BlueprintReadOnly, Category = "Vital Attributes", ReplicatedUsing=OnRep_Mana)
+	FGameplayAttributeData Mana;
+	
+	UPROPERTY(BlueprintReadOnly, Category = "Vital Attributes", ReplicatedUsing=OnRep_MaxMana)
+	FGameplayAttributeData MaxMana;
+	
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData& OldHealth) const;
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const;
+	UFUNCTION()
+	void OnRep_Mana(const FGameplayAttributeData& OldMana) const;
+	UFUNCTION()
+	void OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const;
+
+	ATTRIBUTE_ACCESSORS(URPGAttributeSet, Health);
+	ATTRIBUTE_ACCESSORS(URPGAttributeSet, MaxHealth);
+	ATTRIBUTE_ACCESSORS(URPGAttributeSet, Mana);
+	ATTRIBUTE_ACCESSORS(URPGAttributeSet, MaxMana);
 };
